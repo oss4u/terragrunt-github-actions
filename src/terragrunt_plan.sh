41ca61dab07b00add11180476775c90f3ac4aae6
Benjamin Chung <benjamin.chung@celink.com>
2021-02-26 09:04:37 -0800

    append working dir

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -3,7 +3,7 @@
 function terragruntPlan {
   # Gather the output of `terragrunt plan`.
   echo "plan: info: planning Terragrunt configuration in ${tfWorkingDir}"
-  planOutput=$(${tfBinary} plan-all --terragrunt-working-dir terragrunt --terragrunt-non-interactive 2>&1)
+  planOutput=$(${tfBinary} plan-all --terragrunt-working-dir ${tfWorkingDir}/terragrunt --terragrunt-non-interactive 2>&1)
   planExitCode=${?}
   planHasChanges=false
   planCommentStatus="Failed"
